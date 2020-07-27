e46d021e56d470ba5b2e85af1cba1e471c0d4b46
rogulinilya <rogulinilya@gmail.com>
2020-07-27 15:51:20 +0300

    Updates

---
+++ precache-manifest.f9f41879092870a3f3e70907a008ed31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9702b62e0371f39074de273d13c414ef",
+    "url": "/audio_bot_app/index.html"
+  },
+  {
+    "revision": "3cabd6c664d7bb7b7509",
+    "url": "/audio_bot_app/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "c522ce0d6ac7fac5d47c",
+    "url": "/audio_bot_app/static/css/main.f63d6529.chunk.css"
+  },
+  {
+    "revision": "3cabd6c664d7bb7b7509",
+    "url": "/audio_bot_app/static/js/2.d88e6dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/audio_bot_app/static/js/2.d88e6dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c522ce0d6ac7fac5d47c",
+    "url": "/audio_bot_app/static/js/main.19e96874.chunk.js"
+  },
+  {
+    "revision": "33fb1dddd933644e746f",
+    "url": "/audio_bot_app/static/js/runtime-main.9fdd43eb.js"
+  }
+]);
